bb672fa78380cb347510d27d42507bd796168415
Gyana Ranjan Nath <Gyana.Ranjan.Nath.consultant@heart.org>
2024-08-23 11:29:51 +0530

    Created CURD api

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
 var objectId = mongodb.ObjectId;
-var getDB = require('../common/db_connection');
+var getDB = require('../common/db-connection');
 
-/* GET users listing. */
+/* GET Api Testing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Testing...');
 });
 
-router.get('/list', async function(req, res, next) {
+/* GET All User */ 
+router.get('/list', async function(req, res, next){
   try{
     const db = await getDB();
     const collection = db.collection('user');
@@ -18,6 +19,67 @@ router.get('/list', async function(req, res, next) {
   }catch(ex){
     res.send(ex.message);
   }
-});
+})
+
+/* Save User Details */
+router.post('/save', async function(req, res, next) {
+    try{
+        const data = req.body.data;
+        const db = await getDB();
+        const collection = db.collection("user");
+        const result = await collection.insertOne(data)
+        res.send(result);
+    }catch(error){
+        res.send(error.message);
+    }
+})
+
+/* Update User Details - All fields */
+
+router.put('/update-all-data', async function(req, res, next) {
+    try{
+      const id = req.query.id;
+      const data = req.body.data;
+
+      const db = await getDB();
+      const collection = db.collection("user");
+      const result = await collection.updateOne({_id: objectId.createFromHexString(id)}, {$set:data});
+      res.send(result);
+  
+    }catch(error){
+      res.send(error.message);
+    }
+})
+
+router.patch('/update', async function(req, res, next) {
+  try{
+    const id = req.query.id;
+    const data = req.body.data;
+
+    const db = await getDB();
+    const collection = db.collection("user");
+    const result = await collection.updateOne({_id: objectId.createFromHexString(id)}, {$set:data});
+    res.send(result);
+
+  }catch(error){
+    res.send(error.message);
+  }
+})
+
+/* Delete User Details */
+
+router.delete('/delete/:id', async function(req, res, next) {
+  try{
+    const id = req.params.id;
+
+    const db = await getDB();
+    const collection = db.collection("user");
+    const result = await collection.deleteOne({_id: objectId.createFromHexString(id)});
+    res.send(result);
+    
+  }catch(error){
+    res.send(error.message);
+  } 
+})
 
 module.exports = router;
